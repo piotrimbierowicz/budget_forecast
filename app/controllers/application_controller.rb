c350317d98f3cbe280c002b370326607a8a3b537
piotrimbierowicz <piotr.imbierowicz@gmail.com>
2014-07-13 04:09:46 +0200

    auto locale - not tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter do
+    I18n.locale = I18nData.country_code(request.location.country) unless user_signed_in?
+  end
+
 end

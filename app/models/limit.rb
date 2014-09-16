63e029bfee2310d45701cc33e0ae45c0ab20b936
piotrimbierowicz <piotr.imbierowicz@gmail.com>
2014-09-16 17:28:44 +0200

    limit stats

--- app/models/limit.rb
+++ app/models/limit.rb
@@ -2,7 +2,7 @@ class Limit < ActiveRecord::Base
 
   PERIODS = [Monthly = 'monthly']
 
-  monetize :value_cents
+  monetize :value_cents, with_model_currency: :value_currency
 
   belongs_to :account
   validates_presence_of :account
@@ -19,6 +19,25 @@ class Limit < ActiveRecord::Base
 
   def current_month_limit_left
     self.value - self.current_month_schedules_value - self.current_month_spendings_value
+  end
+
+  def current_month_daily_limit
+    left_days = (Date.current.end_of_month - Date.current).to_i
+    current_month_limit_left / left_days
+  end
+
+  def current_month_primary_daily_limit
+    left_days = (Date.current.end_of_month - Date.current.beginning_of_month).to_i
+    (self.value - current_month_schedules_value) / left_days
+  end
+
+  def current_month_daily_balance
+    current_month_daily_limit - current_month_primary_daily_limit
+  end
+
+  def current_month_monthly_balance
+    days = (Date.current - Date.current.beginning_of_month).to_i
+    current_month_daily_balance * days
   end
 
 end
